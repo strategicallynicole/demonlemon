932f8d409c0a30e4f2483a3fc90f22f10bb2a53b
Strategically Nicole <36245900+strategicallynicole@users.noreply.github.com>
2020-05-13 19:42:45 -0400

    up

---
+++ assets/js/pp.js
@@ -0,0 +1,21 @@
+/*$black = (document).ready(function () {
+  
+    $("div.lazy").lazyload({
+       effect : "fadeIn"
+   });
+     $('#pagepiling').pagepiling({
+         verticalCentered: true,
+         css3: false,
+         onLeave: function (index, nextIndex, direction) {
+              //reaching our last section? The one with our normal site?
+             if (nextIndex == 5) {
+                 $('#pp-nav').fadeOut();
+             }
+  
+             //leaving our last section? The one with our normal site?
+             if (index == 5) {
+                 $('#pp-nav').fadeIn();
+             }
+         },
+     });
+ });*/
